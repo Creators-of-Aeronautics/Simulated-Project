ca8ab94f7235aa53eb4c90626684cc92b4d4e798
Ocelot <FinntheRaider@gmail.com>
2026-04-23 12:54:56 -0600

    Add abstraction for wired capability

--- simulated/neoforge/src/main/java/dev/simulated_team/simulated/neoforge/service/compat/NeoForgeSimPeripheralService.java
+++ simulated/neoforge/src/main/java/dev/simulated_team/simulated/neoforge/service/compat/NeoForgeSimPeripheralService.java
@@ -1,11 +1,9 @@
 package dev.simulated_team.simulated.neoforge.service.compat;
 
+import dan200.computercraft.api.network.wired.WiredElement;
 import dan200.computercraft.api.network.wired.WiredElementCapability;
 import dan200.computercraft.api.peripheral.IPeripheral;
 import dan200.computercraft.api.peripheral.PeripheralCapability;
-import dev.simulated_team.simulated.compat.computercraft.wired.DockingConnectorWiredElementImpl;
-import dev.simulated_team.simulated.content.blocks.docking_connector.DockingConnectorBlock;
-import dev.simulated_team.simulated.index.SimBlockEntityTypes;
 import dev.simulated_team.simulated.service.compat.SimPeripheralService;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityType;
@@ -14,35 +12,41 @@ import net.neoforged.neoforge.capabilities.RegisterCapabilitiesEvent;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Function;
 import java.util.function.Supplier;
 
 public class NeoForgeSimPeripheralService implements SimPeripheralService {
 
-	private static final List<Peripheral<BlockEntity>> PERIPHERALS = new ArrayList<>();
+    private static final List<Entry<BlockEntity, IPeripheral>> PERIPHERALS = new ArrayList<>();
+    private static final List<Entry<BlockEntity, WiredElement>> WIRED_ELEMENTS = new ArrayList<>();
 
-	@Override
-	@SuppressWarnings("unchecked")
-	public <T extends BlockEntity> void addPeripheral(final Supplier<BlockEntityType<T>> typeSupplier, final Function<T, IPeripheral> peripheralFunction) {
-		PERIPHERALS.add((Peripheral<BlockEntity>) new Peripheral<>(typeSupplier, peripheralFunction));
-	}
+    @Override
+    @SuppressWarnings("unchecked")
+    public <T extends BlockEntity> void addPeripheral(final Supplier<BlockEntityType<T>> typeSupplier, final CapabilityGetter<T, IPeripheral> getter) {
+        PERIPHERALS.add((Entry<BlockEntity, IPeripheral>) new Entry<>(typeSupplier, getter));
+    }
 
-	@SubscribeEvent
-	public static void registerCapabilities(final RegisterCapabilitiesEvent event) {
-		for (final Peripheral<BlockEntity> peripheral : PERIPHERALS) {
-			event.registerBlockEntity(PeripheralCapability.get(), peripheral.typeSupplier.get(), (be, direction) ->
-				peripheral.peripheralFunction().apply(be)
-			);
-		}
+    @Override
+    @SuppressWarnings("unchecked")
+    public <T extends BlockEntity> void addWired(Supplier<BlockEntityType<T>> typeSupplier, CapabilityGetter<T, WiredElement> getter) {
+        WIRED_ELEMENTS.add((Entry<BlockEntity, WiredElement>) new Entry<>(typeSupplier, getter));
+    }
 
-		event.registerBlockEntity(WiredElementCapability.get(), SimBlockEntityTypes.DOCKING_CONNECTOR.get(), (e, d) -> {
-			if (e.getBlockState().getValue(DockingConnectorBlock.FACING) == d)
-				return null;
-			return (DockingConnectorWiredElementImpl) e.ccWiredElement;
-		});
-	}
+    @SubscribeEvent
+    public static void registerCapabilities(final RegisterCapabilitiesEvent event) {
+        for (final Entry<BlockEntity, IPeripheral> entry : PERIPHERALS) {
+            event.registerBlockEntity(PeripheralCapability.get(), entry.typeSupplier.get(), (be, direction) ->
+                    entry.peripheralFunction().get(be, direction)
+            );
+        }
 
-	private record Peripheral<T extends BlockEntity>(Supplier<BlockEntityType<T>> typeSupplier, Function<T, IPeripheral> peripheralFunction) {
+        for (final Entry<BlockEntity, WiredElement> entry : WIRED_ELEMENTS) {
+            event.registerBlockEntity(WiredElementCapability.get(), entry.typeSupplier.get(), (be, direction) ->
+                    entry.peripheralFunction().get(be, direction)
+            );
+        }
+    }
 
-	}
+    private record Entry<T extends BlockEntity, V>(Supplier<BlockEntityType<T>> typeSupplier,
+                                                   CapabilityGetter<T, V> peripheralFunction) {
+    }
 }
